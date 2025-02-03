120a2e32c25883013b4cb8ac25bd18443accaf3f
israel3107 <inavlop3107@g.educaand.es>
2025-02-03 12:38:45 +0100

    Update querubin.js

--- querubin.js
+++ querubin.js
@@ -1,8 +1,7 @@
 document.write("br");
 var numero = prompt ("Escribe un numero del 1 al 100");
         numero = Number(numero);
-      var i = 0;
-        var suma = 0;
+ 
       while (numero<1 || numero >100) 
               {
             var numero = prompt (" error, te he dicho que escribas un numero del 1 al 100");
