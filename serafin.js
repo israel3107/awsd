c39b723b44f6fe49759a0e937e18f19ca8243881
israel3107 <inavlop3107@g.educaand.es>
2025-02-03 12:39:36 +0100

    Update serafin.js

--- serafin.js
+++ serafin.js
@@ -2,9 +2,7 @@ document.write("br");
 var numero = prompt ("Escribe un numero del 1 al 100");
         numero = Number(numero);
    
-        var a = 0;
-        var b = 1;
-        var c = 0;
+
       while (numero<1 || numero >100) 
               {
             var numero = prompt (" error, te he dicho que escribas un numero del 1 al 100");
