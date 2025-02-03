1c87ea99595a7610aad5c30590bbe1b8a59e3c80
israel3107 <inavlop3107@g.educaand.es>
2025-02-03 12:40:39 +0100

    Delete querubin.js

--- querubin.js
+++
@@ -1,9 +0,0 @@
-document.write("br");
-var numero = prompt ("Escribe un numero del 1 al 100");
-        numero = Number(numero);
- 
-      while (numero<1 || numero >100) 
-              {
-            var numero = prompt (" error, te he dicho que escribas un numero del 1 al 100");
-              }
-document.write("br");
