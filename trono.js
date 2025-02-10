555e20867c254b85fece3d1b1f8313a8f9f88e5f
israel3107 <inavlop3107@g.educaand.es>
2025-02-10 12:18:30 +0100

    Create trono.js

---
+++ trono.js
@@ -0,0 +1,9 @@
+fuction trono(min,max) {
+var numero;
+do {
+  numero = prompt("ingresar un numero entre " + min + " y " + max + ":");
+  numero = Number(numero);
+} while (numero < min || numero > max);
+
+  return numero;
+}
