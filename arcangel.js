94fc8b0402ed5f2cf6a376373d0f4ec12e07f643
israel3107 <inavlop3107@g.educaand.es>
2025-02-17 12:15:22 +0100

    Create arcangel.js

---
+++ arcangel.js
@@ -0,0 +1,16 @@
+function arcangel (numero)
+  {
+    var primo
+    var i
+    
+for(i=numero-1; i>=2; i=i-1 )
+{
+if ( numero % i ==0 )
+{
+  primo = false
+}
+  
+}
+    primo = true;
+    return primo ;
+  }
