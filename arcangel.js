2fbd3277e9f566ffff5e3011cfa6635669996700
israel3107 <inavlop3107@g.educaand.es>
2025-02-19 13:27:55 +0100

    Update arcangel.js

--- arcangel.js
+++ arcangel.js
@@ -1,16 +1,17 @@
 function arcangel (numero)
   {
-    var primo
-    var i
+    var primo;
+    var i;
     
 for(i=numero-1; i>=2; i=i-1 )
 {
-if ( numero % i ==0 )
+if ( numero % i == 0 )
 {
   primo = false;
+  return primo;
 }
   
 }
     primo = true;
-    return primo ;
+    return primo;
   }
