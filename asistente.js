69030837ddac9cfc426691b48a97b42f2d289300
israel3107 <inavlop3107@g.educaand.es>
2025-02-24 12:07:08 +0100

    Update asistente.js

--- asistente.js
+++ asistente.js
@@ -1,1 +1,17 @@
-
+function asistente (numero)
+  {
+    var par;
+    var i;
+    
+for(i=numero-1; i>=2; i=i-1 )
+{
+if ( numero % i == 0 )
+{
+  par = false;
+  return primo;
+}
+  
+}
+    par = true;
+    return par;
+  }
