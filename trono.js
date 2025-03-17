3a1f6e521b7822785a5118d5f1cff14ac70b5741
israel3107 <inavlop3107@g.educaand.es>
2025-03-17 12:39:09 +0100

    Update trono.js

--- trono.js
+++ trono.js
@@ -7,3 +7,14 @@ do {
 
   return numero;
 }
+
+
+function tronosuperior (texto,min,max) {
+var numero;
+do {
+  numero = prompt("ingresar un numero entre " + min + " y " + max + ":");
+  numero = Number(numero);
+} while (numero < min || numero > max);
+
+  return numero;
+}
